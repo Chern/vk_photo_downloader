cc31fa6a47e6223fb06713785f1e4dcf1f847051
Oleksandr Chernihov <o.chernihov@gmail.com>
2016-01-08 13:19:24 +0200

    unhide input params

--- vk_photo_downloader.py
+++ vk_photo_downloader.py
@@ -2,9 +2,9 @@
 
 import argparse
 import multiprocessing
+import os
 import requests
 import sys
-from os import path, makedirs
 
 API_URL = 'https://api.vk.com/method'
 API_VERSION = '5.42'
@@ -41,22 +41,22 @@ def decode_input(value):
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('owner', help='name or id of user or community', type=decode_input)
-    parser.add_argument('-u', help='specify that owner is user', action='store_true', dest='source_is_user')
+    parser.add_argument('-u', help='specify that owner is user', action='store_true', dest='owner_is_user')
     parser.add_argument('-a', '--albums', nargs='*', type=int,
                         help='specify album ids to download. if it is empty and `--download-all` is not provided, '
                              'user albums list will be printed')
     parser.add_argument('--download-all', help='download all available photos', action='store_true')
     parser.add_argument('-p', '--path', help='specify path to save photos', type=decode_input,
-                        default=path.join(path.dirname(path.abspath(__file__)), 'download/'))
+                        default=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'download/'))
     return parser
 
 
 def get_download_dir(dir_path, subdir=None):
-    abs_path = path.abspath(dir_path)
+    abs_path = os.path.abspath(dir_path)
     if subdir is not None:
-        abs_path = path.join(abs_path, subdir)
-    if not path.exists(abs_path):
-        makedirs(abs_path)
+        abs_path = os.path.join(abs_path, subdir)
+    if not os.path.exists(abs_path):
+        os.makedirs(abs_path)
     return abs_path
 
 
@@ -71,24 +71,24 @@ def downloader(bits):
             f.write(chunk)
 
 
-def download_photos(**kwargs):
-    method, params = 'groups.getById', {'group_id': kwargs['owner']}
-    if kwargs['source_is_user']:
-        method, params = 'users.get', {'user_ids': kwargs['owner']}
+def download_photos(owner_id, album_ids=None, owner_is_user=False, download_all=False, path=None):
+    method, params = 'groups.getById', {'group_id': owner_id}
+    if owner_is_user:
+        method, params = 'users.get', {'user_ids': owner_id}
 
     try:
         owner_info = request_api(method, params=params)[0]
     except VKException:
-        print(u'Can\'t find owner with name or id {}'.format(kwargs['owner']))
+        print(u'Can\'t find owner with name or id {}'.format(owner_id))
     else:
         owner_id = owner_info['id']
-        if not kwargs['source_is_user']:
+        if not owner_is_user:
             owner_id = '-{}'.format(owner_id)
 
         raw_albums = request_api('photos.getAlbums', params={'owner_id': owner_id})
         albums = {album['id']: album['title'] for album in raw_albums['items']}
 
-        if not (kwargs['albums'] or kwargs['download_all']):
+        if not (album_ids or download_all):
             print('Album list\n\nid\t\ttitle')
             print('-' * 80)
             for album_id, album_title in albums.items():
@@ -100,11 +100,11 @@ def download_photos(**kwargs):
 
         existing_album_ids = albums.keys()
 
-        if kwargs['download_all']:
+        if download_all:
             required_album_ids = existing_album_ids
         else:
             required_album_ids, invalid_album_ids = [], []
-            for album_id in kwargs['albums']:
+            for album_id in album_ids:
                 if album_id in existing_album_ids:
                     required_album_ids.append(album_id)
                 else:
@@ -115,7 +115,7 @@ def download_photos(**kwargs):
 
         queue = []
         for album_id in required_album_ids:
-            download_dir = get_download_dir(kwargs['path'], albums[album_id])
+            download_dir = get_download_dir(path, albums[album_id])
 
             print(u'Album {} will be saved to {}'.format(album_id, download_dir))
 
@@ -137,4 +137,6 @@ def download_photos(**kwargs):
 if __name__ == '__main__':
     args_parser = create_parser()
     args = args_parser.parse_args()
-    download_photos(**vars(args))
+
+    download_photos(args.owner, album_ids=args.albums, owner_is_user=args.owner_is_user,
+                    download_all=args.download_all, path=args.path)
